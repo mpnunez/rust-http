d6811dd43f1e398935fe66b11269f62d88a162a2
Marcel Nunez <marcelpnunez@gmail.com>
2024-12-08 14:58:50 -0800

    Implement trait for http client

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 use tokio;
-use reqwest;
+use reqwest::{Client,Response,Error};
+use async_trait::async_trait;
+
+#[async_trait]
+trait HttpGetter {
+    async fn get_http_response(&self, url: &str) -> Result<Response, Error>;
+}
+
+#[async_trait]
+impl HttpGetter for Client {
+    async fn get_http_response(&self, url: &str) -> Result<Response, Error> {
+        self.get(url).send().await
+    }
+}
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
+async fn main() -> Result<(), Error> {
+    let client = Client::new();
     let result = make_request("http://example.com", &client).await?;
     println!("Response: {}", result);
     Ok(())
 }
 
-async fn make_request(url: &str, client: &reqwest::Client) -> Result<String, reqwest::Error> {
-
-    let response = client
-        .get(url)
-        .send()
-        .await?
-        .error_for_status()?;
-
+async fn make_request(url: &str, client: &impl HttpGetter) -> Result<String, Error> {
+    let response = client.get_http_response(url).await?.error_for_status()?;
     let body = response.text().await?;
     Ok(body)
 }
@@ -27,14 +34,14 @@ mod tests {
 
     #[tokio::test]
     async fn test_make_good_request(){
-        let client = reqwest::Client::new();
+        let client = Client::new();
         let result = make_request("http://example.com", &client).await;
         assert!(result.is_ok());
     }
 
     #[tokio::test]
     async fn test_make_bad_request(){
-        let client = reqwest::Client::new();
+        let client = Client::new();
         let result = make_request("http://does-not-exist.com", &client).await;
         assert!(result.is_err());
     }
