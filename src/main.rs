5bcf3233b5761ef976dd9e62b1914a2a24745794
Marcel Nunez <marcelpnunez@gmail.com>
2024-12-11 09:32:10 -0800

    Mock works

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use tokio;
 use reqwest::{Client,Error};
 use async_trait::async_trait;
-use mockall::automock;
+#[cfg(test)]
+use mockall::{automock, predicate::*};
 
 // Order of macros matters. See https://medium.com/vortechsa/mocking-in-async-rust-248b012c5e99
 #[cfg_attr(test, automock)]
@@ -43,19 +44,30 @@ mod tests {
         //let mock_client = MockHttpGetter();
         let mock_client = Client::new();
         return mock_client;
+        // Make this fixture return one mock client with the correct logic for 
+        // Make it mutable so that tests can say what they expect from it
     }
 
     #[rstest]
     #[tokio::test]
-    async fn test_make_good_request(client_for_test: Client){
-        let mock_client = MockHttpBodyGetter::new().expect_get_http_response_body().returning(|_| Ok("<!doctype html></html>".to_string()));
-        let result = make_request("http://example.com", &client_for_test).await;
-        assert!(result.is_ok());
+    async fn test_make_good_request(/*client_for_test: Client*/){
+        let mut mock_client = MockHttpBodyGetter::new();
+        mock_client.expect_get_http_response_body()
+            .times(1)
+            .returning(|_| Ok("<!doctype html></html>".to_string()));
+        assert_eq!(mock_client.get_http_response_body("argument").await.unwrap(),"<!doctype html></html>");
+        //let result = make_request("http://example.com", &mock_client).await;
+        //assert!(result.is_ok());
     }
 
     #[rstest]
     #[tokio::test]
-    async fn test_make_bad_request(client_for_test: Client){
+    async fn test_make_bad_request(/*client_for_test: Client*/){
+        let client_for_test = Client::new();
+        /*
+        let mock_client = MockHttpBodyGetter::new()
+            .expect_get_http_response_body()
+            .returning(|_| Error::new( reqwest::error::Kind, )); */
         let result = make_request("http://does-not-exist.com", &client_for_test).await;
         assert!(result.is_err());
     }
