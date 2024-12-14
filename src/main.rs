aaf6b217795d97fdc1e04dadf85e9442d613047c
Marcel Nunez <marcelpnunez@gmail.com>
2024-12-14 12:58:55 -0800

    Successful mocks

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,61 @@
 use tokio;
-use reqwest::{Client,Error};
+use reqwest;
 use async_trait::async_trait;
 #[cfg(test)]
 use mockall::{automock, predicate::*};
+
+use std::fmt;
+
+
+// Define our error types. These may be customized for our error handling cases.
+// Now we will be able to write our own errors, defer to an underlying error
+// implementation, or do something in between.
+#[derive(Debug, Clone)]
+struct NetworkError;
+
+// Generation of an error is completely separate from how it is displayed.
+// There's no need to be concerned about cluttering complex logic with the display style.
+//
+// Note that we don't store any extra info about the errors. This means we can't state
+// which string failed to parse without modifying our types to carry that information.
+impl fmt::Display for NetworkError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "invalid first item to double")
+    }
+}
+
+impl From<reqwest::Error> for NetworkError {
+    fn from(_t: reqwest::Error) -> NetworkError {
+        NetworkError
+    }
+}
 
 // Order of macros matters. See https://medium.com/vortechsa/mocking-in-async-rust-248b012c5e99
 #[cfg_attr(test, automock)]
 #[async_trait]
 trait HttpBodyGetter {  // Need to mock this trait for unit tests
-    async fn get_http_response_body(&self, url: &str) -> Result<String, Error>;
+    async fn get_http_response_body(&self, url: &str) -> Result<String, NetworkError>;
 }
 
 #[async_trait]
-impl HttpBodyGetter for Client {
-    async fn get_http_response_body(&self, url: &str) -> Result<String, Error> {
-        self.get(url)
+impl HttpBodyGetter for reqwest::Client {
+    async fn get_http_response_body(&self, url: &str) -> Result<String, NetworkError> {
+        Ok(self.get(url)
             .send().await?
             .error_for_status()?
-            .text().await
+            .text().await?)
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let client = Client::new();
+async fn main() -> Result<(), NetworkError> {
+    let client = reqwest::Client::new();
     let result = make_request("http://example.com", &client).await?;
     println!("Response: {}", result);
     Ok(())
 }
 
-async fn make_request(url: &str, client: &impl HttpBodyGetter) -> Result<String, Error> {
+async fn make_request(url: &str, client: &impl HttpBodyGetter) -> Result<String, NetworkError> {
     let body = client.get_http_response_body(url).await?;
     Ok(body)
 }
@@ -48,7 +74,7 @@ mod tests {
                 match url {
                     "http://example.com" => Ok("<!doctype html></html>".to_string()),
                     "https://ringsdb.com/api/public/card/01005" => Ok("{name\":\"Legolas}".to_string()),
-                    _ => Ok("reqwest error".to_string())
+                    _ => Err(NetworkError)
                 }
             );
         return mock_client;
@@ -67,12 +93,7 @@ mod tests {
 
     #[rstest]
     #[tokio::test]
-    async fn test_make_bad_request(/*client_for_test: Client*/){
-        let client_for_test = Client::new();
-        /*
-        let mock_client = MockHttpBodyGetter::new()
-            .expect_get_http_response_body()
-            .returning(|_| Error::new( reqwest::error::Kind, )); */
+    async fn test_make_bad_request(client_for_test: impl HttpBodyGetter){
         let result = make_request("http://does-not-exist.com", &client_for_test).await;
         assert!(result.is_err());
     }
