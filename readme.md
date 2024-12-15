9dd5917e9e9c8a215ea5924660eacfccd42f04b2
Marcel Nunez <marcelpnunez@gmail.com>
2024-12-14 18:54:13 -0800

    Reduce to simplest example

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Trait Mocking in Rust
+
+Demonstration of how to use traits to mock dependencies in
+Rust during unit testing.
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,46 @@
 use tokio;
 use reqwest;
-use async_trait::async_trait;
-#[cfg(test)]
-use mockall::{automock, predicate::*};
-
-use std::fmt;
-
-
-// Define our error types. These may be customized for our error handling cases.
-// Now we will be able to write our own errors, defer to an underlying error
-// implementation, or do something in between.
-#[derive(Debug, Clone)]
-struct NetworkError;
-
-// Generation of an error is completely separate from how it is displayed.
-// There's no need to be concerned about cluttering complex logic with the display style.
-//
-// Note that we don't store any extra info about the errors. This means we can't state
-// which string failed to parse without modifying our types to carry that information.
-impl fmt::Display for NetworkError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid first item to double")
-    }
-}
-
-impl From<reqwest::Error> for NetworkError {
-    fn from(_t: reqwest::Error) -> NetworkError {
-        NetworkError
-    }
-}
-
-// Order of macros matters. See https://medium.com/vortechsa/mocking-in-async-rust-248b012c5e99
-#[cfg_attr(test, automock)]
-#[async_trait]
-trait HttpBodyGetter {  // Need to mock this trait for unit tests
-    async fn get_http_response_body(&self, url: &str) -> Result<String, NetworkError>;
-}
-
-#[async_trait]
-impl HttpBodyGetter for reqwest::Client {
-    async fn get_http_response_body(&self, url: &str) -> Result<String, NetworkError> {
-        Ok(self.get(url)
-            .send().await?
-            .error_for_status()?
-            .text().await?)
-    }
-}
 
 #[tokio::main]
-async fn main() -> Result<(), NetworkError> {
-    let client = reqwest::Client::new();
-    let result = returns_html("http://example.com", &client).await?;
+async fn main() -> Result<(), reqwest::Error> {
+    let result = returns_html("http://example.com").await?;
     println!("Response: {}", result);
     Ok(())
 }
 
-async fn returns_html(url: &str, client: &impl HttpBodyGetter) -> Result<bool, NetworkError> {
-    let body = client.get_http_response_body(url).await?;
+async fn returns_html(url: &str) -> Result<bool, reqwest::Error> {
+    
+    let client = reqwest::Client::new();
+    let body = client.get(url)
+            .send().await?
+            .error_for_status()?
+            .text().await?;
     Ok(body.starts_with("<!doctype html>"))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rstest::{rstest,fixture};
 
-    #[fixture]
-    pub fn client_for_test() -> impl HttpBodyGetter {
-        let mut mock_client = MockHttpBodyGetter::new();
-        mock_client.expect_get_http_response_body()
-            .returning(
-                |url|
-                match url {
-                    "http://example.com" => Ok("<!doctype html></html>".to_string()),
-                    "https://ringsdb.com/api/public/card/01005" => Ok("{name\":\"Legolas}".to_string()),
-                    _ => Err(NetworkError)
-                }
-            );
-        return mock_client;
-    }
-
-    #[rstest]
     #[tokio::test]
-    async fn test_get_html(client_for_test: impl HttpBodyGetter){
+    async fn test_get_html(){
 
-        let result = returns_html("http://example.com", &client_for_test).await;
+        let result = returns_html("http://example.com").await;
         assert!(result.is_ok());
         assert!(result.unwrap());
     }
 
-    #[rstest]
     #[tokio::test]
-    async fn test_get_non_html(client_for_test: impl HttpBodyGetter){
+    async fn test_get_non_html(){
 
-        let result = returns_html("https://ringsdb.com/api/public/card/01005", &client_for_test).await;
+        let result = returns_html("https://ringsdb.com/api/public/card/01005").await;
         assert!(result.is_ok());
         assert!(!result.unwrap());
     }
 
-    #[rstest]
     #[tokio::test]
-    async fn test_bad_request(client_for_test: impl HttpBodyGetter){
-        let result = returns_html("http://does-not-exist.com", &client_for_test).await;
+    async fn test_bad_request(){
+        let result = returns_html("http://does-not-exist.com").await;
         assert!(result.is_err());
     }
 }
